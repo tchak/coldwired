5139ac4642db5c19ea3c2e6d79df63ed08a64dc2
Paul Chavard <pro@paul.chavard.net>
2022-06-18 08:36:00 +0200

    fix(app): demo page

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
-import { createBrowserTurboRouter } from '..';
+import { createBrowserTurboRouter } from '../src';
 
 const routes = [
   { path: '/', id: 'root', handle: { _loader: true } },
   { path: '/about', id: 'about', handle: { _loader: true } },
 ];
 
-createBrowserTurboRouter({ routes });
+createBrowserTurboRouter({ routes, debug: true });

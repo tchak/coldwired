526f96dd1c3f8982f1924d72eaf86eaf82f82d02
Paul Chavard <pro@paul.chavard.net>
2024-02-23 21:39:23 +0100

    chore: remove router and update dependencies

--- packages/router/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './application';
-export * from './schema';
-export { ContentType } from './data';
-
-export type { Fetcher, Navigation, AgnosticRouteObject as RouteObject } from '@remix-run/router';
